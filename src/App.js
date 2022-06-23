8ab1c2ec658b1efd07f88a7042de097a2e678b03
JehadMR <106768742+JehadMR@users.noreply.github.com>
2022-06-23 23:33:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {CssBaseline, Grid} from "@material-ui/core";
-
+import Box from '@mui/material/Box';
 import Header from "./components/Header/Header";
 import Map from "./components/Map/Map";
 import List from "./components/List/List";
@@ -12,9 +12,19 @@ import ActivityInfo from "./components/ActivityInfo/ActivityInfo";
 const App = () => {
     return(
         <>
+         
          <CssBaseline/>
+         <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="10vh"
+            maxWidth="90%"
+            >
+
          <Header/>
-         <Grid container spacing={3} style={{ width: '100%' }}>
+         </Box>
+         <Grid container spacing={3}             display="flex" justifyContent="center" alignItems="center" >
             <Grid item xs={12} md={4}>
                 <List/>
             </Grid>
